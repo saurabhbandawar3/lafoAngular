0e779aca1748cf30717850b257f5bf6cc1a05b58
Saurabh Bandawar <saurabhbandawar3@gmail.com>
2018-03-19 22:49:37 -0400

    sign in code added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../models/user';
+import { AngularFireAuth } from 'angularfire2/auth';
+import {Router} from '@angular/router';
+import {SearchFoundComponent} from '../search-found/search-found.component';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  user = {} as User;
+  constructor( private aAuth: AngularFireAuth,
+               private router: Router) { }
 
   ngOnInit() {
   }
-
+  async login(user) {
+    try {
+      const result = await this.aAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+      console.log(result);
+      if (result) {
+        console.log('user logged in');
+        this.router.navigate(['/slost']);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
