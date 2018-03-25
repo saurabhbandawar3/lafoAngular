0ae18e0019211b9db6b9a7b67f190dad957f9ab0
rutujab1 <rutujabandawar@gmail.com>
2018-03-26 01:12:34 +0530

    changed config and about

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,8 +1,23 @@
 export const firebaseConfig = {
-    apiKey: 'AIzaSyADZk7uNvdDsZ-E17eI3hwr6DCnqOdC1SY',
-    authDomain: 'mangulardb.firebaseapp.com',
-    databaseURL: 'https://mangulardb.firebaseio.com',
-    projectId: 'mangulardb',
-    storageBucket: 'mangulardb.appspot.com',
-    messagingSenderId: '401862798601'
-};
+    apiKey: 'AIzaSyAInC7Kbt49Q70JiNMX-2Oydz3Ta655_64',
+    authDomain: 'myproject-d2801.firebaseapp.com',
+    databaseURL: 'https://myproject-d2801.firebaseio.com',
+    projectId: 'myproject-d2801',
+    storageBucket: 'myproject-d2801.appspot.com',
+    messagingSenderId: '1039481095935'
+  };
+
+
+
+
+
+
+
+// {
+//     apiKey: 'AIzaSyADZk7uNvdDsZ-E17eI3hwr6DCnqOdC1SY',
+//     authDomain: 'mangulardb.firebaseapp.com',
+//     databaseURL: 'https://mangulardb.firebaseio.com',
+//     projectId: 'mangulardb',
+//     storageBucket: 'mangulardb.appspot.com',
+//     messagingSenderId: '401862798601'
+// };
